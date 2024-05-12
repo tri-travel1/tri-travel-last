ee79a88b4c9f765a474b60803da245a5ecd0109a
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-05-12 09:03:27 -0700

    server fixed  i hope

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = 8008;
 const prisma =  require('./db')
 const logins = require('./routes/login')
 const profile = require('./routes/profile')
+const bookings = require('./routes/booking')
+
 
 
 
@@ -15,14 +17,16 @@ app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname,'./public')))
 app.use('/login',logins);
 app.use('/profile',profile);
+app.use('/booking', bookings);
+
 
 
 app.get('/explore',async(_,res)=>{
-  const cars = await prisma.customer.findMany();
+  const cars = await prisma.car.findMany();
   const hotels = await prisma.hotel.findMany();
   const tours = await prisma.tourGuide.findMany();
   const sits = await prisma.site.findMany();
-  
+  console.log({cars, hotels, tours, sits})
   return res.render('explore',{
     cars,hotels,tours,sits
   })
